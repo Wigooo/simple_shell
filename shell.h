7e084d2b71d65c0651a42f86ba6b94bb6c9a4dae
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-10-17 19:32:42 -0400

    shell.h and main.c modified

--- shell.h
+++ shell.h
@@ -91,11 +91,11 @@ typedef struct passinfo
 	int histcount;
 } info;
 
-#define INFO_INIT  \
-	{          \
-		NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL,\
-		NULL, NULL, 0, 0, NULL, \
-			0, 0, 0     \
+#define INFO_INIT\
+	{\
+		NULL, NULL, 0, NULL, 0, 0, 0, NULL, NULL, NULL, \
+			NULL, NULL, 0, 0, 0,\
+			NULL, 0, 0\
 	}
 
 /**
