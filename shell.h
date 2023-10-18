3a94a9af41a7c78a576acdd5adc81a7705cebf73
Mohamed <abdoashraf973@gmial.com>
2023-10-17 21:24:02 -0700

    updated shell

--- shell.h
+++ shell.h
@@ -91,12 +91,9 @@ typedef struct passinfo
 	int histcount;
 } info;
 
-#define INFO_INIT  \
-	{          \
-		NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL,\
-		NULL, NULL, 0, 0, NULL, \
-			0, 0, 0     \
-	}
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
 
 /**
  * struct builtin - contains builtin string and related function
